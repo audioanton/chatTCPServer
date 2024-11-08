a5a9797eb8b364063e0ea9abcd97d37338745c5a
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-11-08 11:33:14 +0100

    Added client GUI

---
+++ src/Client/GUI.java
@@ -0,0 +1,55 @@
+package Client;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI {
+    JFrame frame;
+    JPanel mainPanel;
+    JPanel topPanel;
+    JPanel centerPanel;
+    JPanel bottomPanel;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JButton disconnectButton;
+    JTextField textField;
+    String username;
+
+    public GUI(String username) {
+        this.username = username;
+        frame = new JFrame();
+        mainPanel = new JPanel();
+        topPanel = new JPanel();
+        centerPanel = new JPanel();
+        bottomPanel = new JPanel();
+        textArea  = new JTextArea();
+        scrollPane  = new JScrollPane(textArea);
+        disconnectButton = new JButton("Disconnect");
+        textField = new JTextField("Write something here...");
+    }
+
+    public void init() {
+        frame.setSize(600,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.add(mainPanel);
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(topPanel, BorderLayout.NORTH);
+        mainPanel.add(centerPanel, BorderLayout.CENTER);
+        mainPanel.add(bottomPanel, BorderLayout.SOUTH);
+
+        topPanel.setLayout(new GridLayout(1,1));
+        topPanel.add(disconnectButton);
+
+        centerPanel.setLayout(new GridLayout(1,1));
+        centerPanel.add(scrollPane);
+        textArea.append(username + " CONNECTED");
+
+        bottomPanel.setLayout(new GridLayout(1,1));
+        bottomPanel.add(textField);
+
+    }
+}
---
+++ src/Client/Main.java
@@ -0,0 +1,9 @@
+package Client;
+
+public class Main {
+    public static void main(String[] args) {
+        GUI gui = new GUI("Andreas");
+        gui.init();
+    }
+
+}
